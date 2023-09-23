b89d9ef314a5c1d4c466902bac09b6955f715f95
Arush Kumar <138035241+ashes002@users.noreply.github.com>
2023-09-23 15:41:13 +0530

    metrics & crf update

--- metrics.py
+++ metrics.py
@@ -1,1 +1,36 @@
 
+from functools import wraps
+from itertools import chain
+
+def flatten(y):
+    """
+    Flatten a list of lists.
+    >>> flatten([[1,2], [3,4]])
+    [1, 2, 3, 4]
+    """
+    return list(chain.from_iterable(y))
+
+def _flattens_y(func):
+    @wraps(func)
+    def wrapper(y_true, y_pred, *args, **kwargs):
+        y_true_flat = flatten(y_true)
+        y_pred_flat = flatten(y_pred)
+        return func(y_true_flat, y_pred_flat, *args, **kwargs)
+    return wrapper
+
+@_flattens_y
+def flat_classification_report(y_true, y_pred, labels=None, **kwargs):
+    """
+    Return classification report for sequence items.
+    """
+    from sklearn import metrics
+    return metrics.classification_report(y_true, y_pred, labels, **kwargs)
+
+
+@_flattens_y
+def flat_f1_score(y_true, y_pred, **kwargs):
+    """
+    Return F1 score for sequence items.
+    """
+    from sklearn import metrics
+    return metrics.f1_score(y_true, y_pred, **kwargs)
